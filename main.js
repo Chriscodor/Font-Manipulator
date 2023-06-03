0113393004738376deccf73424ef9abc4920b69b
Chriscodor <115654875+Chriscodor@users.noreply.github.com>
2023-06-03 18:55:46 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,46 +1,41 @@
+
 noseX = 0;
 noseY = 0;
+diff = 0;
+leftWristX = 0;
+rightWristX = 0;
 
-clownose = "";
-
-function preload(){
-    clownose = loadImage("https://i.postimg.cc/QC1xnPh9/nose1.gif");
-}
 function setup(){
-canvas = createCanvas(500,500);
-canvas.center();
-video = createCapture(VIDEO);
-video.hide();
-poseNet = ml5.poseNet(video, modelLoaded);
-poseNet.on('pose', gotPoses);
-}
+    video = createCapture(VIDEO);
+    video.size(600,550)
+    video.position(100,200)
 
+    canvas = createCanvas(600,550)
+    canvas.position(1000,200)
+
+    poseNet = ml5.poseNet(video,modelLoaded)
+    poseNet.on('pose',gotPoses)
+}
 function modelLoaded(){
-    console.log("model is loaded🎊🎊")
-}
-
-
-function gotPoses(results){
-    if(results.length>0){
-        console.log(results);
-        noseX = results[0].pose.nose.x
-        noseY = results[0].pose.nose.y
-        console.log(noseX,"this is nose x");
-        console.log(noseY,"this is nose y");
-    }
+    console.log("Model is ready")
 }
 
 function draw(){
-image(video,0,0 ,500,500);
-fill(255,0,0);
-stroke(255,0,0)
-//circle(noseX-85,noseY,40);
-image(clownose,noseX-118,noseY-25,70,70);
-
-
+   background("#5bbd91") 
+    fill("purple");
+    stroke("purple")
+    textSize(diff)
+    text("Christopher",noseX,noseY)
 }
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
 
-function take_snapshot(){
-save("clown.png");
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+
+        diff = leftWristX-rightWristX;
+    }
 }
-
